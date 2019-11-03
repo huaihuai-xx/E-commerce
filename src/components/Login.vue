6418d99a5f48a7baf1050c00ac608770e543bb16
坏坏的xx <939637841@qq.com>
2019-11-03 14:50:02 +0800

    完成登录

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,7 +3,7 @@
       <div class='login_box'>
           <!-- 头像区域 -->
           <div class="avatar_box">
-              <img src="../assets/logo.png" alt="">
+              <img src="../assets/person.jpg" alt="">
           </div>
           <!-- 登录表单区域 -->
           <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
@@ -17,7 +17,7 @@
             </el-form-item>
             <!-- 按钮 -->
              <el-form-item class="btns">
-                  <el-button type="primary" round>登录</el-button>
+                  <el-button @click="login" type="primary" round>登录</el-button>
                   <el-button @click="resetLoginForm" type="info" round>重置</el-button>
             </el-form-item>
           </el-form>
@@ -31,8 +31,8 @@ export default {
     return {
       // 这是登录表单的数据绑定对象
       loginForm: {
-        username: '',
-        password: ''
+        username: 'admin',
+        password: '123456'
       },
       // 表单的验证规则对象
       loginFormRules: {
@@ -53,6 +53,20 @@ export default {
     // 点击重置按钮，重置登录表单
     resetLoginForm () {
       this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        // 1. 将登录成功之后的 token ，保存到客户端 sessionStorage 中
+        //  1.1 项目中出了登录之外的其他API接口，必须在登录之后才能访问
+        //  1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStore中
+        window.sessionStorage.setItem('token', res.data.token)
+        // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push('/home')
+      })
     }
   }
 }
@@ -60,7 +74,7 @@ export default {
 
 <style lang='less' scoped>
 .login_container {
-    background-color: skyblue;
+    background-color: #2b2b2b;
     height: 100%;
 }
 
--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import './plugins/element.js'
 import './assets/css/global.css'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
-
+import axios from 'axios'
+// 配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   router,
