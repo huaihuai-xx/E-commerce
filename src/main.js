c81d919781f168bfde710d4d003bf113f07c3185
坏坏的xx <939637841@qq.com>
2019-11-06 09:43:46 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(VueRouter)
 
@@ -9,10 +11,16 @@ const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    { path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [{ path: '/welcome', component: Welcome },
+        { path: '/users', component:Users } 
+      ] 
+    }
   ]
 })
-// 挂载路由导航守卫
+// 挂载路由导航守卫 
 router.beforeEach((to, form, next) => {
   // to 将要访问的路径
   // from 代表从哪个路径跳转而来
