288a9abbbae327ce1f909cb0cd0f59f775cf4158
坏坏的xx <939637841@qq.com>
2019-11-12 18:55:30 +0800

    订单功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -37,7 +37,8 @@ import { Button,
   CheckboxGroup,
   Upload
 } from 'element-ui'
-
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -73,5 +74,7 @@ Vue.use(Step)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import GoodList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
+import Order from '../components/order/order.vue'
 
 Vue.use(VueRouter)
 
@@ -27,7 +28,8 @@ const router = new VueRouter({
         { path: '/categories', component:Cate },
         { path: '/params', component:Params },
         { path: '/goods', component:GoodList },
-        { path: '/goods/add', component:Add }
+        { path: '/goods/add', component:Add },
+        { path: '/orders', component:Order }
       ] 
     }
   ]
